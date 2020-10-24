eb7bccdec5423a35a9d029426aa4dbd2c231836e
Humberto Alcocer <humbertowoody@gmail.com>
2020-10-23 22:03:54 -0500

    Update tarea 2

--- Tareas/tarea-2/README.md
+++ Tareas/tarea-2/README.md
@@ -4,8 +4,7 @@ Un programa que realiza suma y resta de números racionales.
 
 El código está basado en uno proporcionado por el profesor durante la clase,
 realicé algunas adecuaciones para que sea _compliant_ con C99 ya que en algunos
-casos específicos (como el mío) los compiladores ya tienen banderas para 
+casos específicos (como el mío) los compiladores ya tienen banderas para
 tomar Warnings como Errores y no compilaba.
 
 22 de Octubre de 2020
-
--- Tareas/tarea-2/racionales.c
+++ Tareas/tarea-2/racionales.c
@@ -1,75 +1,84 @@
 /**
- * Tarea: 1
- * Tema: Suma y Resta de dos números racionales.
+ * Tarea: 2
+ * Tema: Suma y Resta de dos números racionales en C.
  * Materia: Programación Orientada a Objetos.
  * Profesor: Roberto Tecla
  * Alumno: Humberto Alejandro Ortega Alcocer (2016630495) <hortegaa1500@alumno.ipn.mx>
  * Grupo: 2CM1
  * Fecha: 22 de Octubre del 2020
  */
-#include <stdio.h>   // Operaciones de E/S
-#include <stdlib.h>  // Para usar malloc a partir de C99.
+#include <stdio.h>  // Operaciones de E/S
+#include <stdlib.h> // Para usar malloc a partir de C99.
 
 // Typedef + struct para ahorrar pasos.
-typedef struct racional {
-    int num, den;
+typedef struct racional
+{
+  int num, den;
 } Racional;
 
 /*Pre-condición: den  <> 0 */
-Racional *crearRacional(int num, int den){
+Racional *crearRacional(int num, int den)
+{
   Racional *nvo;
-  
+
   // No puede ser el denominador 0.
-  if(den==0) return (Racional *)NULL;
-  
-  // Asignamos la memoria para el nuevo elemeto.
-  nvo=(Racional *)malloc(sizeof(Racional));
-  
-  // Si no hay espacio en memoria, imprimimos mensaje y salimos.
-  if(!nvo){
-    printf("No hay memoria para crear Racional.\n"); 
+  if (den == 0)
     return (Racional *)NULL;
-  }  
- 
-  // Realizamos la asignación de los datos. 
-  nvo->num=num;
-  nvo->den=den;
-  
+
+  // Asignamos la memoria para el nuevo elemeto.
+  nvo = (Racional *)malloc(sizeof(Racional));
+
+  // Si no hay espacio en memoria, imprimimos mensaje y salimos.
+  if (!nvo)
+  {
+    printf("No hay memoria para crear Racional.\n");
+    return (Racional *)NULL;
+  }
+
+  // Realizamos la asignación de los datos.
+  nvo->num = num;
+  nvo->den = den;
+
   // Regresamos el apuntador con el número racional recién creado.
   return nvo;
 }
 
 /* Pre-condición: r ha sido creado. */
-void asignar(Racional * r, int num, int den);
+void asignar(Racional *r, int num, int den);
 
 /* Pre-condición: r ha sido creado. */
-int numerador( Racional *r){
-	return r->num;
+int numerador(Racional *r)
+{
+  return r->num;
 }
 
 /* Pre-condición: r ha sido creado. */
-int denominador(Racional *r){
-	return r->den;
+int denominador(Racional *r)
+{
+  return r->den;
 }
 
 /* Pre-condición:  r1 y r2 han sido creados. */
-Racional *multiplicar(Racional *r1, Racional  *r2){
-	return crearRacional(r1->num*r2->num,r1->den*r2->den);
+Racional *multiplicar(Racional *r1, Racional *r2)
+{
+  return crearRacional(r1->num * r2->num, r1->den * r2->den);
 }
 
 /* Comprueba si dos números racionales son iguales */
-int igual(Racional *r1, Racional  *r2){
-  return r1->num*r2->den == r1->den*r2->num;
+int igual(Racional *r1, Racional *r2)
+{
+  return r1->num * r2->den == r1->den * r2->num;
 }
 
 /* Imprime un número racional */
-void imprimir(void *q){
-	Racional *r=(Racional *)q;
-	printf("[%d / %d]", r->num,r->den);
+void imprimir(void *q)
+{
+  Racional *r = (Racional *)q;
+  printf("[%d / %d]", r->num, r->den);
 }
 
 /* Suma de dos números racionales */
-Racional* sumar(Racional *r1, Racional *r2)
+Racional *sumar(Racional *r1, Racional *r2)
 {
   return crearRacional(r1->num + r2->num, r1->den * r2->den);
 }
@@ -81,21 +90,22 @@ Racional *restar(Racional *r1, Racional *r2)
 }
 
 /* Función principal */
-int main(){
+int main()
+{
   // Variables locales.
-  Racional *r1,*r2, *res_mult, *res_suma, *res_resta;
+  Racional *r1, *r2, *res_mult, *res_suma, *res_resta;
 
   // Creamos el primer número racional y lo imprimimos.
-	r1=crearRacional(2,3);
-	imprimir(r1);
-	
+  r1 = crearRacional(2, 3);
+  imprimir(r1);
+
   // Creamos el segundo número racional y lo imprimimos.
-  r2=crearRacional(3,2);
-	imprimir(r2);
-	
+  r2 = crearRacional(3, 2);
+  imprimir(r2);
+
   // Realizamos la multiplicación y mostramos el resultado.
   res_mult = multiplicar(r1, r2);
-	imprimir(res_mult);
+  imprimir(res_mult);
 
   // Resta de dos números racionales.
   res_resta = restar(r1, r2);
